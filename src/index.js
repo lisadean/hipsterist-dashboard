55450e903ffd45de61d7b1c49a36139b2862b5de
Beth Damato <bethadamato@gmail.com>
2018-07-30 18:53:34 -0400

    finished the styling of the pomodoro

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
+// global.jquery = require('jquery');
+// require('bootstrap');
+// import 'bootstrap/dist/css/bootstrap.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-import 'bootstrap/dist/css/bootstrap.css';
-require('bootstrap');
-global.jquery = require('jquery');
 
 ReactDOM.render(
     <App />,
