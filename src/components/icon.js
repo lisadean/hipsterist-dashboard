8c7367305797cbd00f60ee741731e6ef55357aa0
Aylin DeBruyne <adebruyne88@gmail.com>
2018-07-31 13:58:26 -0400

    added a few images to weather icon

--- src/components/icon.js
+++ src/components/icon.js
@@ -2,6 +2,14 @@ import React from "react";
 const Icon = ({ icon }) => {
   return (
     <div>
+      {/*clear day  */}
+      {/* <img src="http://weddingflowersbyjuliarose.com/wp-content/uploads/2016/12/Tommys-flower-beard_LoRes05-683x1024.jpg" /> */}
+      {/* clear night */}
+      {/* <img src="https://i.pinimg.com/originals/09/d7/01/09d701ad5aa681dc2281d63ee58493d3.jpg" />*/}
+      {/* partly-cloudy-day */}
+      {/* <img src="https://images.unsplash.com/photo-1527309195845-a455c2efbc94?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f832a6a69f52f5ab39a3103df6690aa1&auto=format&fit=crop&w=800&q=60" /> */}
+      {/* partly-cloudy-night */}
+      {/* <img src="https://images.unsplash.com/photo-1452979081267-f541411cb48e?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=3ee7b756cc40f94b526daddd2d6ade0b&auto=format&fit=crop&w=800&q=60" />*/}
       <img src={_whatsTheImage(icon)} alt="weather icon" />
     </div>
   );
@@ -11,16 +19,16 @@ const _whatsTheImage = iconName => {
   switch (iconName) {
     //clear-day
     case "clear-day":
-      return "";
+      return "http://weddingflowersbyjuliarose.com/wp-content/uploads/2016/12/Tommys-flower-beard_LoRes05-683x1024.jpg";
     //clear-night
     case "clear-night":
-      return "";
+      return "https://i.pinimg.com/originals/09/d7/01/09d701ad5aa681dc2281d63ee58493d3.jpg";
     //partly-cloudy-day
     case "partly-cloudy-day":
-      return "";
+      return "https://images.unsplash.com/photo-1527309195845-a455c2efbc94?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f832a6a69f52f5ab39a3103df6690aa1&auto=format&fit=crop&w=800&q=60";
     //partly-cloudy-night
     case "partly-cloudy-night":
-      return "";
+      return "https://images.unsplash.com/photo-1452979081267-f541411cb48e?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=3ee7b756cc40f94b526daddd2d6ade0b&auto=format&fit=crop&w=800&q=60";
     //cloudy
     case "cloudy":
       return "";
