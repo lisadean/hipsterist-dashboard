d996111cb98ce181fe9f64f3010ffaaab0a1edc9
Beth Damato <bethadamato@gmail.com>
2018-07-30 21:56:51 -0400

    working on the timerconfig logic to get the count in sync

--- src/Pomodoro/Timerconfig/timerconfig.js
+++ src/Pomodoro/Timerconfig/timerconfig.js
@@ -1,38 +1,76 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const TimerConfig = (props) => (
-    <div className="row">
-    <h3 className="text-primary">Set Timer</h3>
+class TimerConfig extends Component {
+    constructor() {
+        super();
 
-    <div className="form-group">
-        <div className="col-3">
-            <label htmlFor="hours">Hours</label>
-        </div>
-        <div className="col-9">
-            <input id="hours" className="form-control" type="number" defaultValue={props.hours} />
-        </div>
-    </div>
+        this.handleChange = this.handleChange.bind(this);
+    }
 
-    <div className="form-group">
-        <div className="col-3">
-            <label htmlFor="hours">Minutes</label>
-        </div>
-        <div className="col-9">
-            <input id="minutes" className="form-control" type="number" defaultValue={props.minutes} />
-        </div>
-    </div>
 
-    <div className="form-group">
-        <div className="col-3">
-            <label htmlFor="hours">Seconds</label>
-        </div>
-        <div className="col-9">
-            <input id="seconds" className="form-control" type="number" defaultValue={props.seconds} />
-        </div>
-    </div>
+    handleChange(ev) {
+        const newBaseTime = this.props.baseTime;
+    
+        if (ev.target.id === 'hours') newBaseTime.subtract(newBaseTime.get('hour'), 'hours').add(parseInt(ev.target.value, 10), 'hours');
+        if (ev.target.id === 'minutes') newBaseTime.subtract(newBaseTime.get('minutes'), 'minutes').add(parseInt(ev.target.value, 10), 'minutes');
+        if (ev.target.id === 'seconds') newBaseTime.subtract(newBaseTime.get('seconds'), 'seconds').add(parseInt(ev.target.value, 10), 'seconds');
+    
+        this.props.setBaseTime(newBaseTime);
+    }
 
-</div>
 
-);
+    render() {
+        return( 
+            <div className="row">
+                <h3 className="text-primary">Set Timer</h3>
+                <div className="form-group">
+                    <div className="col-3">
+                        <label htmlFor="hours">Hours</label>
+                    </div>
+                    <div className="col-9">
+                        <input 
+                        id="hours" 
+                        className="form-control" 
+                        type="number" 
+                        defaultValue={this.props.baseTime.get('hours')} 
+                        onChange={this.handleChange}
+                    />
+                    </div>
+                </div>
+
+                <div className="form-group">
+                    <div className="col-3">
+                        <label htmlFor="hours">Minutes</label>
+                    </div>
+                    <div className="col-9">
+                        <input 
+                        id="minutes" 
+                        className="form-control" 
+                        type="number" 
+                        defaultValue={this.props.baseTime.get('minutes')} 
+                        onChange={this.handleChange}
+                    />
+                    </div>
+                </div>
+
+                <div className="form-group">
+                    <div className="col-3">
+                        <label htmlFor="hours">Seconds</label>
+                    </div>
+                    <div className="col-9">
+                        <input 
+                        id="seconds" 
+                        className="form-control" 
+                        type="number" 
+                        defaultValue={this.props.baseTime.get('seconds')} 
+                        onChange={this.handleChange}
+                    />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
 
 export default TimerConfig;
