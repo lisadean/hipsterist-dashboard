f1f26925177320d7141519955eb680ba348fca6b
Aylin DeBruyne <adebruyne88@gmail.com>
2018-08-01 09:58:24 -0400

    adding styles to main App

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        {/* Header */}
+        <header id="showcase" className="grid">
+          <div className="bg-image" />
+          <h1>Welcome to Hipsterist</h1>
+          <p>This is an obscure dashboard you've probably never heard of.</p>
+        </header>
         <div className=" ">
           <div className="weather  col-sm">
             <Weather />
